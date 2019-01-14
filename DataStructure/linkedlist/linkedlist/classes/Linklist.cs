01c1d38db298db9f5496af7ff3f99855cdfd4100
shalom <shalom.belaineh@gmail.com>
2019-01-14 10:52:49 -0800

    ADDED PRINT

---
+++ DataStructure/linkedlist/linkedlist/classes/Linklist.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linkedlist
+{
+    public class Linklist
+    {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+
+        public void Insert(int value)
+        { 
+        
+        Node insertedNode = new Node(value);
+        insertedNode.Next=Head;
+            Head =insertedNode;
+
+
+     }
+        public bool Includes(int values)
+        {
+            Current = Head;
+            while (Current.Next != null)
+            {
+                if (Current.value == VALUE)
+                {
+                    return true;
+                }
+                Current = Current.NEXT;
+
+            }
+            if (Current.Value == Value)
+            {
+                return true;
+            }
+            return false;
+        }
+
+            public void print()
+            {
+                Current = Head;
+                while(Current != null)
+                {
+                    Console.Write(Current.VAlue + " ");
+                    Current = Current.Next;
+                }
+            }
+        }
+       
+
+
+
+
+    }
+}
---
+++ DataStructure/linkedlist/linkedlist/classes/Node.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linkedlist.classes
+{
+    class Node
+    {
+    }
+}
