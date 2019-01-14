b8d007d66cfb3ffccefc3af6825afe69858b3cab
shalom <shalom.belaineh@gmail.com>
2019-01-14 10:31:50 -0800

    setup

---
+++ Challenges/codechallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codechallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
