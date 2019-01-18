96f70ba8743a21f6a7a08a912e926e6d005387c3
shalina2 <shalom.belaineh@gmail.com>
2019-01-18 12:48:39 -0800

    tests pass

--- Challenges/MergeList/MergeList test/UnitTest1.cs
+++ Challenges/MergeList/MergeList test/UnitTest1.cs
@@ -10,18 +10,54 @@ namespace MergeList_test
         [Fact]
         public void FirstListLong()
         {
-        LList list1 = new LList();
+            LList list1 = new LList();
 
-        list1.Insert(1);
-        list1.Insert(2);
-        list1.Insert(3);
-        list1.Insert(4);
+            list1.Insert(1);
+            list1.Insert(2);
+            list1.Insert(4);
             LList list2 = new LList();
             list2.Insert(5);
-            list2.Insert(10);
-           Program.MergeList (list1, list2);
+            list2.Insert(9);
+            Program.MergeList(list1, list2);
             Assert.Equal(5, list1.GetValue(1));
         }
+        [Fact]
+
+        public void havesamelength()
+
+        {
+
+            LList list1 = new LList();
+            list1.Insert(1);
+            list1.Insert(2);
+            LList list2 = new LList();
+            list2.Insert(5);
+            list2.Insert(9);
+            Program.MergeList(list1, list2);
+            Assert.Equal(1, list1.GetValue(1));
+
+
+        }
+
+        [Fact]
+        public void list2islonger()
+
+        {
+
+            LList list1 = new LList();
+            list1.Insert(1);
+            list1.Insert(2);
+            LList list2 = new LList();
+            list2.Insert(5);
+            list2.Insert(9);
+            list2.Insert(99);
+           Program.MergeList(list1, list2);          
+            Assert.Equal(9, list1.GetValue(1));
+
+
+
+        }
+
     }
 }
 
