5692257a7a394cd1b069c5c8c67d0495d4ae667c
shalina2 <shalom.belaineh@gmail.com>
2019-01-22 02:37:04 -0800

    Added test

---
+++ Challenges/BinarySearch/quiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
