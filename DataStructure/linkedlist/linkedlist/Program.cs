aef09f4bcd9a4d8c57f2df39de220e8b04399ee0
shalina2 <shalom.belaineh@gmail.com>
2019-01-23 03:09:20 -0800

    added test

--- DataStructure/linkedlist/linkedlist/Program.cs
+++ DataStructure/linkedlist/linkedlist/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using linkedlist.classes;
 using LinkedList.Classes;
 
 namespace linkedlist
@@ -9,34 +10,83 @@ namespace linkedlist
         {
             Console.WriteLine("LinkedList!");
 
-            LList listing = new LList();
-            listing.Insert(2);
-            listing.Insert(4);
-            listing.Insert(6);
-
-            listing.Print();
-
-
-            listing.Append(8);
-            listing.Append(10);
-            listing.Append(12);
-
-            listing.Print();
-
-            listing.InsertBefore(10, 11);
-            listing.Print();
-            listing.InsertAfter(2, 100);
-            listing.Print();
-
-
-            int a = listing.GetValue(13);
-            int b = listing.GetValue(2);
-
-            int c = listing.GetValue(130);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-
+            LList listing1 = new LList();
+            listing1.Insert(2);
+            listing1.Insert(4);
+            listing1.Insert(6);
+            LList listing2 = new LList();
+            listing2.Print();                    
+            listing2.Insert (8);          
+            listing2.Insert(10);
+            listing2.Print();           
+            Console.WriteLine();
+            MergeLists(listing1, listing2);
         }
+
+            public static void MergeLists(LList list1, LList list2)
+
+            {
+            // defining the length of the List.
+                list1.Current = list1.Head;
+                list2.Current = list2.Head;
+                int counter1 = 0;
+                int counter2 = 0;
+                while (list1.Current.Next != null)
+               {
+
+                    list1.Current = list1.Current.Next;
+                    counter1++;
+                }
+
+                counter1++;
+
+                while (list2.Current.Next != null)
+
+                {
+                    list2.Current = list2.Current.Next;            
+                    counter2++;
+                }
+
+                counter2++;
+                //reset the current to the head
+                list1.Current = list1.Head;
+                list2.Current = list2.Head;
+                while (counter1 > counter2 && list1.Current.Next != null)
+                {
+
+                    Node temp = list1.Current.Next;
+                    list1.Current.Next = list2.Current;
+                   list1.Current = list1.Current.Next;
+                   list2.Current = list2.Current.Next;
+                    list1.Current.Next = temp;
+                    list1.Current = list1.Current.Next;
+                }
+
+                while (counter1 == counter2 && list2.Current.Next != null)
+                {
+
+                    Node temp = list1.Current.Next;
+                    list1.Current.Next = list2.Current;
+                    list1.Current = list1.Current.Next;
+                    list2.Current = list2.Current.Next;
+                    list1.Current.Next = temp;
+                    list1.Current = list1.Current.Next;
+                }
+
+                while (counter1 < counter2 && list1.Current.Next != null)
+
+                {
+
+                    Node temp = list1.Current.Next;
+                    list1.Current.Next = list2.Current;
+                    list1.Current = list1.Current.Next;
+                    list2.Current = list2.Current.Next;
+                    list1.Current.Next = temp;
+                    list1.Current = list1.Current.Next;
+                }
+
+                list1.Current.Next = list2.Current;
+                list1.Print();
+            }
     }
 }
