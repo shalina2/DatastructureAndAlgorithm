bd889e84c3c5f8ea0653546a8173dae2a1b8b9a0
shalina2 <shalom.belaineh@gmail.com>
2019-01-23 02:46:05 -0800

    added dequeue and enqueue

---
+++ Challenges/QueueWithStacks/QueueWithStacks/PseudoQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using QueueWithStacks;
+
+namespace QueueWithStacks
+{
+  
+        public class PseudoQueue
+        {
+            public Stack Front { get; set; }
+            public Stack back { get; set; }
+            
+        public PseudoQueue(Stack stack)
+        {
+            Front = stack;
+            back = stack;
+        }
+
+        public void Enqueque(int value)
+        {
+            Stack temp = new Stack();
+            while (Front.Top.Next != null)
+            {
+                Node tempNode = Front.Pop();
+                temp.Push(tempNode.Value);
+            }
+            Node tempNode2 = Front.Pop();
+            temp.Push(tempNode2.Value);
+
+                //Pushing the node to the stack
+
+                Front.Push(value);
+                while(temp.Top.Next != null)
+                {
+                    Node tempNode = temp.Pop();
+                    Front.Push(tempNode.Value);
+                }
+            Node tempNode3 = temp.Pop();
+
+            Front.Push(tempNode3.Value);
+            }
+
+          public void Dequeue()
+        {
+            Node pop = Front.Pop();
+
+            Console.WriteLine(pop.Value);
+          }
+
+        public void Print(Stack stack)
+        {
+            Node current = stack.Top;
+            int counter = 0;
+            while(current.Next != null)
+            {
+                counter++;
+                current = current.Next;
+            }
+            counter++;
+
+            int[] arr = new int[counter];
+            current = stack.Top;
+
+        }
+
+
+        }
+    }
+    
+
---
+++ DataStructure/stackAndQueues/Classes/classes/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stackandqueues.classes
+{
+   public class Stack
+    {
+        public Node Top { get; set; }
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        public Stack() //creataing a new stack so leaving it empty.
+        {
+
+        }
+
+        public Node Pop()
+        {
+            Node temp = Top;
+            Top = Top.Next;
+            temp.Next = null;
+            return temp;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
+        public Node Peek()
+        {
+            return Top;
+        }
+    }
+}
