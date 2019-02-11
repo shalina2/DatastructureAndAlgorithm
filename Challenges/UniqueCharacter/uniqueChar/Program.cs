7e957976d378ebc9631581c3fa96b77be19ac7fb
shalina2 <shalom.belaineh@gmail.com>
2019-02-11 15:12:12 -0800

    workingon unique characters

---
+++ Challenges/UniqueCharacter/uniqueChar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uniqueChar
+{
+    public class Program
+    {
+        static bool AreCharactersUnique(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                for (int j = 0; j < input.Length; j++)
+                    if (input[i] == input[j])
+                        return false;
+            //if all are unique return true
+            return true;
+        }
+        public static void Main()
+        {
+            string input = "ILoveCodefellow";
+            if (AreCharactersUnique(input) == true)
+                Console.WriteLine("The string " + input + " has unique");
+            else
+                Console.WriteLine("The String " + input + " has duplicate");
+
+        }
+
+    }
+}
